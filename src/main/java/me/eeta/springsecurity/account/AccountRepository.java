1a86edd1af9149d394efc56b42e987a056b62325
eeta <hwlim.me@gmail.com>
2021-07-14 11:59:45 +0900

    Update AccountService to implement UserDetailsService using builder pattern

--- src/main/java/me/eeta/springsecurity/account/AccountRepository.java
+++ src/main/java/me/eeta/springsecurity/account/AccountRepository.java
@@ -3,5 +3,5 @@ package me.eeta.springsecurity.account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-    void findByUsername(String username);
+    Account findByUsername(String username);
 }
--- src/main/java/me/eeta/springsecurity/account/AccountService.java
+++ src/main/java/me/eeta/springsecurity/account/AccountService.java
@@ -1,6 +1,7 @@
 package me.eeta.springsecurity.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,8 +14,15 @@ public class AccountService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        accountRepository.findByUsername(username);
+        Account account = accountRepository.findByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException(username);
+        }
 
-        return null;
+        return User.builder()
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
     }
 }
