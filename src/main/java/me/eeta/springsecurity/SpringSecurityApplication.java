64c1788abcfa8bb586d62598a90dfbad27a6a20b
eeta <hwlim.me@gmail.com>
2021-07-15 19:34:11 +0900

    Update Encoding password using bcrypt

--- src/main/java/me/eeta/springsecurity/SpringSecurityApplication.java
+++ src/main/java/me/eeta/springsecurity/SpringSecurityApplication.java
@@ -3,7 +3,7 @@ package me.eeta.springsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
@@ -11,7 +11,7 @@ public class SpringSecurityApplication {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     public static void main(String[] args) {
