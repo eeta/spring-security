359da3f183befab9b1e355457db2c567e402ab5c
eeta <hwlim.me@gmail.com>
2021-07-15 17:45:16 +0900

    Comment explicit declaration on SecurityConfig

--- src/main/java/me/eeta/springsecurity/config/SecurityConfig.java
+++ src/main/java/me/eeta/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package me.eeta.springsecurity.config;
 
+// import me.eeta.springsecurity.account.AccountService;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /*
+    @Autowired
+    AccountService accountService;
+    */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,4 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
     }
+
+    /*
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(accountService);
+    }
+    */
 }
